f3c35044a9d9e7ed62538522e059df40e186a13f
Valentin <valentin.lyash@gmail.com>
2022-01-20 10:51:16 -0500

    building the dayListItem and DayList components to ensure that the props are displayed on the butttons and functionality properly for scheduling according to day/spots availability

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
+
+{/* function that format the props.spots for passing test*/}
+const formatSpots = (spots) => {
+    if (!spots) {
+      return `no spots remaining`;
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    }
+    return `${spots} spots remaining`;
+  };
+
+export default function DayListItem(props) {
+    const spotsReformating=formatSpots(props.spots)
+    console.log(spotsReformating)
+    let dayClass = classNames("day-list__item",{
+        "day-list__item--selected" : props.selected,
+        "day-list__item--full" : !props.spots
+    }) 
+
+  return (
+    <li className = {dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsReformating}</h3>
+    </li>
+  );
+}
