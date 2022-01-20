07ccaa03f766df1110962ffba6715ddcb2a25424
Valentin <valentin.lyash@gmail.com>
2022-01-20 16:08:05 -0500

    building a daylist and daylistitem mapping so that it iterates in array and passes down the props into daylist and then daylistitem to display on the screen and troubleshooting using storybook method

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,30 +1,36 @@
 import React from "react";
+import DayListItem from "components/DayListItem";
 
-export default function DayList(props){
+export default function DayList(props) { //3 props: 1. Array of days 2. name of currently selected day 3. function used to set current day (setDay)
+  const { days } = props;
+  const parsedDay = days.map((day) => { //creates an array of DayListItems
+      return(
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+      )
+  });
+  return <ul>{parsedDay}</ul>;
+}
 
-    return(
-      <ul>
-        <DayListItem 
-          key={props.days[0].id}
-          name={props.days[0].name} 
-          spots={props.days[0].spots} 
-          selected={props.days[0].name === props.day}
-          setDay={props.setDay}  
-        />
-        <DayListItem
-          key={props.days[1].id} 
-          name={props.days[1].name} 
-          spots={props.days[1].spots} 
-          selected={props.days[1].name === props.day}
-          setDay={props.setDay}  
-        />
-        <DayListItem 
-          key={props.days[2].id}
-          name={props.days[2].name}
-          spots={props.days[2].spots} 
-          selected={props.days[2].name === props.day}
-          setDay={props.setDay}  
-        />      
-      </ul>
-    )
-  }
+// import React from "react";
+// import DayListItem from "components/DayListItem";
+
+// {/*create the list of <DayListItem> components */}
+// export default function DayList(props) {
+//   const { days } = props;
+//   const parsedDay = days.map((day) => (
+//     <DayListItem
+//       key={day.id}
+//       name={day.name}
+//       spots={day.spots}
+//       selected={day.name === props.value}
+//       setDay={props.onChange}
+//     />
+//   ));
+//   return <ul>{parsedDay}</ul>;
+// }
